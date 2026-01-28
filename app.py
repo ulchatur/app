16ae61440e9a2b4237b2798661451b5752f3f429
Aditya Singh <aditya.singh2@ab-inbev.com>
2026-01-28 11:34:27 +0530

    Workflow updated

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ DB_CONFIG = {
 }
 
 # --------------------------------------------------
-# DB Connection code updated
+# DB Connection code
 # --------------------------------------------------
 def get_db_connection():
     try:
